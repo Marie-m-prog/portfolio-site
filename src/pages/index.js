ce09fb49365c56eea9df7bde417349499718bff0
marie winther madsen <mariewmadsen@gmail.com>
2020-09-22 18:26:06 +0200

    add font and animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,33 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
+import Fade from 'react-reveal/Fade';
 import SEO from "../components/seo"
 import Header from "../components/header"
 import "../components/styles.css"
 
-const IndexPage = () => (
+const IndexPage = () => {
+  let title = "Hello, my name is Marie";
+
+
+  return (
   <div>
     <SEO title="Home" />
     <section className="landing">
       <Header />
-      <h1>Hello, my name is Marie</h1>
-      <p>{`I’m a full-stack developer graduated from </salt> specialized in Javascript.`}</p>
+        <div className="title-text">
+          <h1>
+            <span>
+              {title.split("").map((char, index) => {
+                return (
+                  <Fade top cascade delay={index*10}><span key={index}>{char}</span></Fade>
+                )
+              })}
+            </span>
+          </h1>
+          <p>{`I’m a full-stack developer graduated from </salt> specialized in Javascript.`}</p>
+        </div>
     </section>
-    <div className="intro">
-      <div className="intro-text">
-        <h1 className="hide">
-          <span className="text">Creating innovation</span>
-        </h1>
-      </div>
-    </div>
     <div className="slider"></div>
   </div>
-)
+)}
 
 export default IndexPage
