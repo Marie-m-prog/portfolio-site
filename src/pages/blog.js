814654637bb2ada0813727bf8b5197e9dd99fd35
Marie <mariewmadsen@gmail.com>
2020-12-13 21:50:58 +0100

    fixed mobile styling issues

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,34 +1,32 @@
 import React from "react"
 import SEO from "../components/seo"
-import { graphql } from "gatsby";
+import { graphql } from "gatsby"
 
 const BlogPage = ({ data }) => {
-	const posts = data.allContentfulBlog.edges;
-
   return (
-	<div>
-    <SEO title="Blog" />
-    <h1>Blog</h1>
-		<ul>{}</ul>
-	</div>
-	)
+    <div>
+      <SEO title="Blog" />
+      <h1>Blog</h1>
+      <ul>{}</ul>
+    </div>
+  )
 }
 
 export default BlogPage
 
 export const postQuery = graphql`
-query BlogQuery {
-	allContentfulBlog(sort: { fields: [publishDate], order: DESC }) {
-    edges {
-      node {
-        date(formatString: "DD/MM/YYYY")
-        slug
-        title
-        content {
-          raw
+  query BlogQuery {
+    allContentfulBlog(sort: { fields: [date], order: DESC }) {
+      edges {
+        node {
+          date(formatString: "DD/MM/YYYY")
+          slug
+          title
+          content {
+            raw
+          }
         }
       }
     }
   }
-}
 `
